1b9464450027c75a717b56b941b4a2bc29b8437b
LizhenQiao <948090427@qq.com>
2022-08-01 23:05:24 -0400

    Format code

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,109 +1,146 @@
 pragma solidity ^0.5.0;
 
 contract Adoption {
-    struct Item{			
+    struct Item {
         string vehicle_brand; // vehicle's brand
-		string vehicle_model; // which specific model is this vehicle
+        string vehicle_model; // which specific model is this vehicle
         string description; // information about a vehicle, e.g. production year, mileage
-        uint buy_now_price;
-		uint starting_price; // starting price
-		uint min_incr; // minimum increment for a bid
-		uint offer_price;  // current price of item
-	}
+        uint256 buy_now_price;
+        uint256 starting_price; // starting price
+        uint256 min_incr; // minimum increment for a bid
+        uint256 offer_price; // current price of item
+    }
 
     address[16] public buyers; // array of ethereum addresses
-    uint constant carCount = 10;
-    uint public numOfPurchases = 0;
-    uint[carCount] public arrayForItems;
-	uint public itemId = 0;
-    mapping(uint => Item) public vehicles; // item hash table
-    mapping(uint => address) public highestOfferers; // highest bidders hash table 
-    event offerPlacement(uint _vehicleId, uint indexed _offerAmount); // Declaring events which help us use ethereum's logging facility
+    uint256 constant carCount = 10;
+    uint256 public numOfPurchases = 0;
+    uint256[carCount] public arrayForItems;
+    uint256 public itemId = 0;
+    mapping(uint256 => Item) public vehicles; // item hash table
+    mapping(uint256 => address) public highestOfferers; // highest bidders hash table
+    event offerPlacement(uint256 _vehicleId, uint256 indexed _offerAmount); // Declaring events which help us use ethereum's logging facility
 
-    function addItem (string memory vehicle_brand, string memory vehicle_model, string memory description, uint buy_now_price, uint starting_price, uint min_incr, uint offer_price) public { 	
-		vehicles[itemId] = Item(vehicle_brand, vehicle_model, description, buy_now_price, starting_price, min_incr, offer_price);
-		highestOfferers[itemId] = address(0);
-		itemId ++;
-	}
-    
-    constructor() public {	
-		addItem("BMW", "M2", "need info here", 40, 20, 2, 20);
-		addItem("Mercedes", "GTR", "need info here", 400, 200, 20, 200);
-		addItem("Honda", "Civic Type-R", "need info here", 300, 150, 35, 150);
-		addItem("Volkswagen", "Golf gti", "need info here", 300, 150, 35, 150);
+    function addItem(
+        string memory vehicle_brand,
+        string memory vehicle_model,
+        string memory description,
+        uint256 buy_now_price,
+        uint256 starting_price,
+        uint256 min_incr,
+        uint256 offer_price
+    ) public {
+        vehicles[itemId] = Item(
+            vehicle_brand,
+            vehicle_model,
+            description,
+            buy_now_price,
+            starting_price,
+            min_incr,
+            offer_price
+        );
+        highestOfferers[itemId] = address(0);
+        itemId++;
+    }
+
+    constructor() public {
+        addItem("BMW", "M2", "need info here", 40, 20, 2, 20);
+        addItem("Mercedes", "GTR", "need info here", 400, 200, 20, 200);
+        addItem("Honda", "Civic Type-R", "need info here", 300, 150, 35, 150);
+        addItem("Volkswagen", "Golf gti", "need info here", 300, 150, 35, 150);
         addItem("BMW", "M3", "need info here", 40, 20, 2, 20);
-		addItem("Mercedes", "c63", "need info here", 400, 200, 20, 200);
-		addItem("Honda", "s2000", "need info here", 300, 150, 35, 150);
-		addItem("Volkswagen", "Golf r", "need info here", 300, 150, 35, 150);
+        addItem("Mercedes", "c63", "need info here", 400, 200, 20, 200);
+        addItem("Honda", "s2000", "need info here", 300, 150, 35, 150);
+        addItem("Volkswagen", "Golf r", "need info here", 300, 150, 35, 150);
         addItem("Ford", "f150", "need info here", 300, 150, 35, 150);
         addItem("Ram", "1500", "need info here", 300, 150, 35, 150);
-	}
-    
+    }
+
     // Adopting a pet
-    function adopt(uint petId) public returns (uint) {
+    function adopt(uint256 petId) public returns (uint256) {
         require(petId >= 0 && petId <= 15);
         buyers[petId] = msg.sender;
-        numOfPurchases = numOfPurchases +1;
+        numOfPurchases = numOfPurchases + 1;
         return petId;
-        }
+    }
+
     // Retrieving the adopters
     function getAdopters() public view returns (address[16] memory) {
         return buyers;
     }
 
-    function getNumOfPurchase() public view returns (uint){
+    function getNumOfPurchase() public view returns (uint256) {
         return numOfPurchases;
     }
 
-    function getItemPrice (uint _itemId) public view returns (uint) {
-		require(_itemId >= 0 && _itemId < carCount, "Item does not exist"); // the item id must be greater than 0 but less or equal to the total count
-		return vehicles[_itemId].offer_price;
-	}
-
-    function getAllPrices() public view returns (uint[carCount] memory) {
-        uint[carCount] memory result;
-        for (uint i=0;i < carCount; i++) {
-            result[i] = this.getItemPrice(i);
-        }
-		return result;
-	}
-
-    function getHighestOfferer() public view returns (address[carCount] memory) {
-		address[carCount] memory arrayOfBidders;
-		for (uint i=0;i < carCount; i++) {
-			arrayOfBidders[i] = highestOfferers[i];
-		}
-		return arrayOfBidders;
-
-	}
-
-
-    function placeNewOffer (uint _vehicleId, uint _offerAmount) public returns (uint) {
-        // Requirements 
-		require(_vehicleId >= 0 && _vehicleId < carCount, "Item trying to play new offer on is invalid"); 	
-		require(check_offer_high_enough (_vehicleId, _offerAmount),"New Offer is lower or equal to the highest bid value");
-		require(check_highest_bidder(_vehicleId, msg.sender), "Person bidding is the highest bidder");
-
-        vehicles[_vehicleId].offer_price = _offerAmount; 
-		highestOfferers[_vehicleId] = msg.sender;
-
-		emit offerPlacement(_vehicleId, _offerAmount); 
-
-        return _vehicleId; // return the item back 	
-
+    function getItemPrice(uint256 _itemId) public view returns (uint256) {
+        require(_itemId >= 0 && _itemId < carCount, "Item does not exist"); // the item id must be greater than 0 but less or equal to the total count
+        return vehicles[_itemId].offer_price;
     }
 
-	function check_offer_high_enough (uint _vehicleId, uint _offerAmount) public view returns (bool) {
-		if (_offerAmount > vehicles[_vehicleId].offer_price) return true;
-		else return false;
-	}
-	
-	function check_highest_bidder (uint _vehicleId, address _wallet) public view returns (bool) {
-		if (_wallet == highestOfferers[_vehicleId]) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+    function getAllPrices() public view returns (uint256[carCount] memory) {
+        uint256[carCount] memory result;
+        for (uint256 i = 0; i < carCount; i++) {
+            result[i] = this.getItemPrice(i);
+        }
+        return result;
+    }
 
+    function getHighestOfferer()
+        public
+        view
+        returns (address[carCount] memory)
+    {
+        address[carCount] memory arrayOfBidders;
+        for (uint256 i = 0; i < carCount; i++) {
+            arrayOfBidders[i] = highestOfferers[i];
+        }
+        return arrayOfBidders;
+    }
+
+    function placeNewOffer(uint256 _vehicleId, uint256 _offerAmount)
+        public
+        returns (uint256)
+    {
+        // Requirements
+        require(
+            _vehicleId >= 0 && _vehicleId < carCount,
+            "Item trying to play new offer on is invalid"
+        );
+        require(
+            check_offer_high_enough(_vehicleId, _offerAmount),
+            "New Offer is lower or equal to the highest bid value"
+        );
+        require(
+            check_highest_bidder(_vehicleId, msg.sender),
+            "Person bidding is the highest bidder"
+        );
+
+        vehicles[_vehicleId].offer_price = _offerAmount;
+        highestOfferers[_vehicleId] = msg.sender;
+
+        emit offerPlacement(_vehicleId, _offerAmount);
+
+        return _vehicleId; // return the item back
+    }
+
+    function check_offer_high_enough(uint256 _vehicleId, uint256 _offerAmount)
+        public
+        view
+        returns (bool)
+    {
+        if (_offerAmount > vehicles[_vehicleId].offer_price) return true;
+        else return false;
+    }
+
+    function check_highest_bidder(uint256 _vehicleId, address _wallet)
+        public
+        view
+        returns (bool)
+    {
+        if (_wallet == highestOfferers[_vehicleId]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,252 +3,291 @@ App = {
   contracts: {},
   account: "0x0000000000000000000000000000000000000000",
 
-  init: async function(obj) {
-        // Load pets.
+  // Render the page.
+  init: async function (obj) {
+    $.getJSON("../cars.json", function (data) {
+      console.log(obj);
+      var carRow = $("#carRow");
+      var carTemplate = $("#carTemplate");
+      for (i = 0; i < data.length; i++) {
+        if (obj == null) {
+          carTemplate.find(".panel-title").text(`Vehicle ${i + 1}`); // auction number as title
+          carTemplate.find("img").attr("src", data[i].picture); // image
+          carTemplate.find(".vehicle_brand").text(data[i].vehicle_brand); // name of item
+          carTemplate.find(".vehicle_model").text(data[i].vehicle_model); // name of item
+          carTemplate
+            .find(".buy_now_price")
+            .text(`ETH ${data[i].buy_now_price}`); // name of item
+          carTemplate.find(".description").text(data[i].description); // decription of the item
+          carTemplate.find(".min_incr").text(`ETH ${data[i].min_incr}`); // minimum increment
+          carTemplate
+            .find(".starting_price")
+            .text(`ETH ${data[i].starting_price}`); // base price
+          carTemplate.find(".btn-buy").attr("data-id", data[i].id);
+          carTemplate.find(".btn-submit").attr("data-id", data[i].id);
 
-        $.getJSON('../cars.json', function(data) {
-            console.log(obj)
-            var carRow = $('#carRow');
-            var carTemplate = $('#carTemplate');
-            for (i = 0; i < data.length; i++) {
-                if (obj == null) {
-                    carTemplate.find('.panel-title').text(`Vehicle ${i+1}`); // auction number as title
-                    carTemplate.find('img').attr('src', data[i].picture); // image 
-                    carTemplate.find('.vehicle_brand').text(data[i].vehicle_brand); // name of item 
-                    carTemplate.find('.vehicle_model').text(data[i].vehicle_model); // name of item 
-                    carTemplate.find('.buy_now_price').text(`ETH ${data[i].buy_now_price}`); // name of item 
-                    carTemplate.find('.description').text(data[i].description); // decription of the item
-                    carTemplate.find('.min_incr').text(`ETH ${data[i].min_incr}`); // minimum increment
-                    carTemplate.find('.starting_price').text(`ETH ${data[i].starting_price}`); // base price
-                    carTemplate.find('.btn-buy').attr('data-id', data[i].id);
-                    carTemplate.find('.btn-submit').attr('data-id', data[i].id);
+          // Creating identifier attributes for HTML elements
+          carTemplate.find(".highest-bid").attr("data-id", data[i].id); // adding attribute to the highest bid so we can dynamically change it
+          carTemplate.find(".btn-submit").attr("data-id", data[i].id); // adding attribute for submit so we can associate itemids to submit buttons
+          carTemplate.find(".ipt-amt").attr("id", `input-amt-${data[i].id}`); // same as above for input amount
+          carTemplate.find(".ipt-amt").attr("step", `${data[i].min_incr}`); // same as above for input amount
+          carTemplate.find(".ipt-amt").attr("min", `${data[i].starting_price}`); // same as above for input amount
+          $("#kaho").append(carTemplate.html());
+        } else {
+          if (
+            data[i].vehicle_brand.toUpperCase().indexOf(obj.toUpperCase()) !==
+              -1 ||
+            data[i].vehicle_model.toUpperCase().indexOf(obj.toUpperCase()) !==
+              -1
+          ) {
+            carTemplate.find(".panel-title").text(`Vehicle ${i + 1}`); // auction number as title
+            carTemplate.find("img").attr("src", data[i].picture); // image
+            carTemplate.find(".vehicle_brand").text(data[i].vehicle_brand); // name of item
+            carTemplate.find(".vehicle_model").text(data[i].vehicle_model); // name of item
+            carTemplate
+              .find(".buy_now_price")
+              .text(`ETH ${data[i].buy_now_price}`); // name of item
+            carTemplate.find(".description").text(data[i].description); // decription of the item
+            carTemplate.find(".min_incr").text(`ETH ${data[i].min_incr}`); // minimum increment
+            carTemplate
+              .find(".starting_price")
+              .text(`ETH ${data[i].starting_price}`); // base price
+            carTemplate.find(".btn-buy").attr("data-id", data[i].id);
+            carTemplate.find(".btn-submit").attr("data-id", data[i].id);
 
-                    // Creating identifier attributes for HTML elements
-                    carTemplate.find('.highest-bid').attr('data-id', data[i].id); // adding attribute to the highest bid so we can dynamically change it
-                    carTemplate.find('.btn-submit').attr('data-id', data[i].id); // adding attribute for submit so we can associate itemids to submit buttons
-                    carTemplate.find('.ipt-amt').attr('id',`input-amt-${data[i].id}`); // same as above for input amount
-                    carTemplate.find('.ipt-amt').attr('step',`${data[i].min_incr}`); // same as above for input amount
-                    carTemplate.find('.ipt-amt').attr('min',`${data[i].starting_price}`); // same as above for input amount
-                    $("#kaho").append(carTemplate.html());
-                } else {
-                    if (data[i].vehicle_brand.toUpperCase().indexOf(obj.toUpperCase()) !== -1 || data[i].vehicle_model.toUpperCase().indexOf(obj.toUpperCase()) !== -1) {
-                        carTemplate.find('.panel-title').text(`Vehicle ${i+1}`); // auction number as title
-                        carTemplate.find('img').attr('src', data[i].picture); // image 
-                        carTemplate.find('.vehicle_brand').text(data[i].vehicle_brand); // name of item 
-                        carTemplate.find('.vehicle_model').text(data[i].vehicle_model); // name of item 
-                        carTemplate.find('.buy_now_price').text(`ETH ${data[i].buy_now_price}`); // name of item 
-                        carTemplate.find('.description').text(data[i].description); // decription of the item
-                        carTemplate.find('.min_incr').text(`ETH ${data[i].min_incr}`); // minimum increment
-                        carTemplate.find('.starting_price').text(`ETH ${data[i].starting_price}`); // base price
-                        carTemplate.find('.btn-buy').attr('data-id', data[i].id);
-                        carTemplate.find('.btn-submit').attr('data-id', data[i].id);
+            // Creating identifier attributes for HTML elements
+            carTemplate.find(".highest-bid").attr("data-id", data[i].id); // adding attribute to the highest bid so we can dynamically change it
+            carTemplate.find(".btn-submit").attr("data-id", data[i].id); // adding attribute for submit so we can associate itemids to submit buttons
+            carTemplate.find(".ipt-amt").attr("id", `input-amt-${data[i].id}`); // same as above for input amount
+            carTemplate.find(".ipt-amt").attr("step", `${data[i].min_incr}`); // same as above for input amount
+            carTemplate
+              .find(".ipt-amt")
+              .attr("min", `${data[i].starting_price}`); // same as above for input amount
+            $("#kaho").append(carTemplate.html());
+            // console.log(carTemplate.html())
+          }
+        }
+      }
+    });
 
-                        // Creating identifier attributes for HTML elements
-                        carTemplate.find('.highest-bid').attr('data-id', data[i].id); // adding attribute to the highest bid so we can dynamically change it
-                        carTemplate.find('.btn-submit').attr('data-id', data[i].id); // adding attribute for submit so we can associate itemids to submit buttons
-                        carTemplate.find('.ipt-amt').attr('id',`input-amt-${data[i].id}`); // same as above for input amount
-                        carTemplate.find('.ipt-amt').attr('step',`${data[i].min_incr}`); // same as above for input amount
-                        carTemplate.find('.ipt-amt').attr('min',`${data[i].starting_price}`); // same as above for input amount
-                        $("#kaho").append(carTemplate.html());
-                        // console.log(carTemplate.html())
-                    }
-                }
+    return await App.initWeb3();
+  },
 
-            }
-        });
-
-        return await App.initWeb3();
-    },
-
-  initWeb3: async function() {
+  initWeb3: async function () {
     // Modern dapp browsers...
-if (window.ethereum) {
-  App.web3Provider = window.ethereum;
-  try {
-    // Request account access
-    await window.ethereum.request({ method: "eth_requestAccounts" });;
-  } catch (error) {
-    // User denied account access...
-    console.error("User denied account access")
-  }
-}
-// Legacy dapp browsers...
-else if (window.web3) {
-  App.web3Provider = window.web3.currentProvider;
-}
-// If no injected web3 instance is detected, fall back to Ganache
-else {
-  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-}
-web3 = new Web3(App.web3Provider);
-
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access");
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:7545"
+      );
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
-  initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
+  initContract: function () {
+    $.getJSON("Adoption.json", function (data) {
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
       var AdoptionArtifact = data;
       App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-    
+
       // Set the provider for our contract
       App.contracts.Adoption.setProvider(App.web3Provider);
 
-      web3.eth.getCoinbase(function(err, account) {
+      web3.eth.getCoinbase(function (err, account) {
         if (err === null) {
           App.account = account;
           $("#account").text(account);
         }
-      })
-    
+      });
+
       // Use our contract to retrieve and mark the adopted pets
-      return App.markPurchase(), App.updateOfferPrice(), App.updateNumOfPurchase(), App.updateHighestOfferer();
+      return (
+        App.markPurchase(),
+        App.updateOfferPrice(),
+        App.updateNumOfPurchase(),
+        App.updateHighestOfferer()
+      );
     });
-    
 
     return App.bindEvents();
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-buy', App.handlePurchase);
-    $(document).on('click', '.btn-submit', App.handleNewOffer);
+  bindEvents: function () {
+    $(document).on("click", ".btn-buy", App.handlePurchase);
+    $(document).on("click", ".btn-submit", App.handleNewOffer);
   },
 
-  markPurchase: function() {
+  markPurchase: function () {
     var adoptionInstance;
 
-    App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
-    
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
-      for (i = 0; i < adopters.length; i++) {
-        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          $('.panel-pet').eq(i).find('button').text('Sold').attr('disabled', true);
+    App.contracts.Adoption.deployed()
+      .then(function (instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.getAdopters.call();
+      })
+      .then(function (adopters) {
+        for (i = 0; i < adopters.length; i++) {
+          if (adopters[i] !== "0x0000000000000000000000000000000000000000") {
+            $(".panel-pet")
+              .eq(i)
+              .find("button")
+              .text("Sold")
+              .attr("disabled", true);
+          }
         }
-      }
-    }).catch(function(err) {
-      console.log(err.message);
-    });
-    
+      })
+      .catch(function (err) {
+        console.log(err.message);
+      });
   },
 
-  handlePurchase: function(event) {
+  handlePurchase: function (event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    var petId = parseInt($(event.target).data("id"));
 
     var adoptionInstance;
 
-    web3.eth.getAccounts(function(error, accounts) {
+    web3.eth.getAccounts(function (error, accounts) {
       if (error) {
         console.log(error);
       }
-    
+
       var account = accounts[0];
-    
-      App.contracts.Adoption.deployed().then(function(instance) {
-        adoptionInstance = instance;
-    
-        // Execute adopt as a transaction by sending account
-        return adoptionInstance.adopt(petId, {from: account});
-      }).then(function(result) {
-        return App.markPurchase(), App.updateNumOfPurchase();
-      }).catch(function(err) {
-        console.log(err.message);
-      });
+
+      App.contracts.Adoption.deployed()
+        .then(function (instance) {
+          adoptionInstance = instance;
+
+          // Execute adopt as a transaction by sending account
+          return adoptionInstance.adopt(petId, { from: account });
+        })
+        .then(function (result) {
+          return App.markPurchase(), App.updateNumOfPurchase();
+        })
+        .catch(function (err) {
+          console.log(err.message);
+        });
     });
-    
   },
 
-  updateNumOfPurchase: function() {
+  updateNumOfPurchase: function () {
     var offerInstance;
-    App.contracts.Adoption.deployed().then(function(instance) {
-      offerInstance = instance;
-      return offerInstance.getNumOfPurchase.call();
-    }).then(function(result) {
-      $(document).find('.num-purchase').text(`${result}`);
-      }).catch(function(err) {
+    App.contracts.Adoption.deployed()
+      .then(function (instance) {
+        offerInstance = instance;
+        return offerInstance.getNumOfPurchase.call();
+      })
+      .then(function (result) {
+        $(document).find(".num-purchase").text(`${result}`);
+      })
+      .catch(function (err) {
         console.log(err.message);
       });
-  
   },
 
-  updateOfferPrice: function() {
+  updateOfferPrice: function () {
     var offerInstance;
-    App.contracts.Adoption.deployed().then(function(instance) {
-      offerInstance = instance;
-      return offerInstance.getAllPrices.call();
-    }).then(function(result) {
-        for (j=0; j < result.length; j++) {
-          $(document).find('.highest-bid').eq(j).text(`ETH ${result[j]}`);
+    App.contracts.Adoption.deployed()
+      .then(function (instance) {
+        offerInstance = instance;
+        return offerInstance.getAllPrices.call();
+      })
+      .then(function (result) {
+        for (j = 0; j < result.length; j++) {
+          $(document).find(".highest-bid").eq(j).text(`ETH ${result[j]}`);
         }
-      }).catch(function(err) {
+      })
+      .catch(function (err) {
         console.log(err.message);
       });
-  
   },
 
-  updateHighestOfferer: function() {
+  updateHighestOfferer: function () {
     var offerInstance;
 
-    App.contracts.Adoption.deployed().then(function(instance) {
-      offerInstance=instance;
+    App.contracts.Adoption.deployed()
+      .then(function (instance) {
+        offerInstance = instance;
 
-      return offerInstance.getHighestOfferer.call(); 
-    }).then(function(result) {
-      for (j=0;j<result.length;j++) {
-        $(document).find('.highest-account').eq(j).text(`${result[j]}`);
-      }
-    }).catch(function(err) {
-      console.log(err.message);
-    })
-  
+        return offerInstance.getHighestOfferer.call();
+      })
+      .then(function (result) {
+        for (j = 0; j < result.length; j++) {
+          $(document).find(".highest-account").eq(j).text(`${result[j]}`);
+        }
+      })
+      .catch(function (err) {
+        console.log(err.message);
+      });
   },
 
-  handleNewOffer: function(event) {
+  handleNewOffer: function (event) {
     event.preventDefault();
 
-    var vehicleId = parseInt($(event.target).data('id'));
+    var vehicleId = parseInt($(event.target).data("id"));
     var offerAmount = parseInt($(`#input-amt-${vehicleId}`).val());
-    
-    var offerInstance;    
+
+    var offerInstance;
     var account = App.account;
-  
-    App.contracts.Adoption.deployed().then(function(instance) {
-      offerInstance = instance;
-  
-      // Execute place bid as a transaction by sending account
-      return offerInstance.placeNewOffer(vehicleId, offerAmount, {from: account});
-    }).then(function(result) {
-      return App.updateNumOfPurchase(), App.updateOfferPrice(), App.updateHighestOfferer();
-    }).catch(function(err) {
-      console.log(err.message);
-    });
-  }
 
+    App.contracts.Adoption.deployed()
+      .then(function (instance) {
+        offerInstance = instance;
 
+        // Execute place bid as a transaction by sending account
+        return offerInstance.placeNewOffer(vehicleId, offerAmount, {
+          from: account,
+        });
+      })
+      .then(function (result) {
+        return (
+          App.updateNumOfPurchase(),
+          App.updateOfferPrice(),
+          App.updateHighestOfferer()
+        );
+      })
+      .catch(function (err) {
+        console.log(err.message);
+      });
+  },
 };
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
 
-$("#search").eq(0).bind('input propertychange', function(e) {
-
-    console.log($(this).val())
+$("#search")
+  .eq(0)
+  .bind("input propertychange", function (e) {
+    console.log($(this).val());
     if ($(this).val() !== "undefined") {
-        $('#kaho .col-sm-6').remove();
-        App.init($(this).val());
+      $("#kaho .col-sm-6").remove();
+      App.init($(this).val());
     }
-})
+  });
 
-
-$('input:radio[name="optradio"]').click(function() {
-    $('#kaho .col-sm-6').remove();
-    var checkValue = $('input:radio[name="optradio"]:checked').val();
-    App.init(checkValue);
+$('input:radio[name="optradio"]').click(function () {
+  $("#kaho .col-sm-6").remove();
+  var checkValue = $('input:radio[name="optradio"]:checked').val();
+  App.init(checkValue);
 });
